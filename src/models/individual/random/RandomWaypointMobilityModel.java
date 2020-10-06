c4fe3aeb214596bd61933ffd634e8be5f2645d73
Sophie <svdeeren@hotmail.com>
2020-10-06 14:44:13 +0200

    finish randomwalk

--- src/models/individual/random/RandomWaypointMobilityModel.java
+++ src/models/individual/random/RandomWaypointMobilityModel.java
@@ -30,36 +30,36 @@ public class RandomWaypointMobilityModel extends RandomIMobilityModel {
 
     @Override
     protected void moveMote(MobilityMote mote) {
-        if (ui.getTimeBased()) {
-            //If amount of time passed (in nanoseconds) is the same as update interval in nanoseconds
-            if (updates * getPeriod() == ui.getUpdateInterval() * Math.pow(10, 9)) {
-                //update speed and direction of mote
-               chooseNewDirection(mote);
-
-                updates = 0;
-            }
-        } else {
-            //if travelled distance is >= update interval distance
-            if (updates * getPeriod() * moteSpeed.get(mote) >= ui.getUpdateInterval()) {
-                chooseNewDirection(mote);
-
-                updates = 0;
-            }
-        }
-
-        //Convert period from nanoseconds to seconds
-        double dist = moteSpeed.get(mote) * getPeriod()/(Math.pow(10, 9));
-
-        double dx = dist * Math.sin(moteDirec.get(mote));
-        double dy = dist * Math.sin(moteDirec.get(mote));
-        mote.translate(dx, dy);
-        updates++;
-    }
-
-    private void chooseNewDirection(MobilityMote mote) {
-        double speed = random.nextDouble() * (ui.getSpeedMax() - ui.getSpeedMin()) + ui.getSpeedMin();
-        moteSpeed.put(mote, speed);
-        double direction = random.nextDouble() * 2 * Math.PI;
-        moteDirec.put(mote, direction);
+//        if (ui.getTimeBased()) {
+//            //If amount of time passed (in nanoseconds) is the same as update interval in nanoseconds
+//            if (updates * getPeriod() == ui.getUpdateInterval() * Math.pow(10, 9)) {
+//                //update speed and direction of mote
+//               chooseNewDirection(mote);
+//
+//                updates = 0;
+//            }
+//        } else {
+//            //if travelled distance is >= update interval distance
+//            if (updates * getPeriod() * moteSpeed.get(mote) >= ui.getUpdateInterval()) {
+//                chooseNewDirection(mote);
+//
+//                updates = 0;
+//            }
+//        }
+//
+//        //Convert period from nanoseconds to seconds
+//        double dist = moteSpeed.get(mote) * getPeriod()/(Math.pow(10, 9));
+//
+//        double dx = dist * Math.sin(moteDirec.get(mote));
+//        double dy = dist * Math.sin(moteDirec.get(mote));
+//        mote.translate(dx, dy);
+//        updates++;
+//    }
+//
+//    private void chooseNewDirection(MobilityMote mote) {
+//        double speed = random.nextDouble() * (ui.getSpeedMax() - ui.getSpeedMin()) + ui.getSpeedMin();
+//        moteSpeed.put(mote, speed);
+//        double direction = random.nextDouble() * 2 * Math.PI;
+//        moteDirec.put(mote, direction);
     }
 }
