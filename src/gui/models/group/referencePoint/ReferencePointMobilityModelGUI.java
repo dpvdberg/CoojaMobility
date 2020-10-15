4698d410bc4479470204bdb606fabc7cd3345757
Dennis van den Berg <dennispvandenberg@gmail.com>
2020-10-15 15:42:53 +0200

    Add getter for ReferencePointMobilityPanel

--- src/gui/models/group/referencePoint/ReferencePointMobilityModelGUI.java
+++ src/gui/models/group/referencePoint/ReferencePointMobilityModelGUI.java
@@ -37,4 +37,8 @@ public class ReferencePointMobilityModelGUI {
     public IndividualMobilityModel getReferencePointMobilityModel() {
         return referencePointMobilityPanel.getActiveModel();
     }
+
+    public ReferencePointMobilityPanel getReferencePointMobilityPanel() {
+        return referencePointMobilityPanel;
+    }
 }
